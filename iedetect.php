7e70bb657ca114c16377d35aeb594f627e8bd936
Christoph S. Ackermann <christoph.ackermann@cubetech.ch>
2013-06-24 16:10:53 +0200

    new tag

--- iedetect.php
+++ iedetect.php
@@ -1,12 +1,12 @@
 <?php
 /*
- Plugin Name: IE Detector
- Description: Erkennt alte Internet Explorer und empfiehlt dem User, einen aktuelleren Browser zu installieren.
- Version: 1.1
- Author: cubetech GmbH
- Plugin URI:  https://github.com/cubetech/wordpress.iedetect
- Author URI:  http://www.cubetech.ch/
- */
+Plugin Name: IE Detector
+Description: Erkennt alte Internet Explorer und empfiehlt dem User, einen aktuelleren Browser zu installieren.
+Version: 1.1
+Author: cubetech GmbH
+Plugin URI:  https://github.com/cubetech/wordpress.iedetect
+Author URI:  http://www.cubetech.ch/
+*/
 
 wp_enqueue_script('ieinfobar', plugins_url(basename(dirname(__FILE__))) . '/ieinfobar.js', array(), false, true);
 wp_localize_script('ieinfobar', 'iedetect', array('url'=>plugins_url(basename(dirname(__FILE__)))));
